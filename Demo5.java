e6ca4d8027ce50e646f6eb9bf6e38dcaf1ffeb62
teja3668 <kandhularaviteja9949@gmail.com>
2025-01-03 22:04:09 +0530

    Add files via upload
    printing hello user happy new year

---
+++ Demo5.java
@@ -0,0 +1,13 @@
+public class Demo5 {
+    public static void main(String[] args){
+        String username = "user001";
+        String pwd = "user_001";
+        while((username == "user001") && (pwd == "user_001")) {
+            System.out.println("hello user happy new year");
+            break;
+        }
+    }
+    
+}
+    
+
