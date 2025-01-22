334b807998cbd2e4ae6a6e3e165b57560c2d0948
teja3668 <kandhularaviteja9949@gmail.com>
2025-01-22 18:50:56 +0530

    Leap year

---
+++ Demo022.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Demo022 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        scanner.close();
+
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
