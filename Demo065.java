a0cacfdd0153ec4ec566842e88a563bdb081f580
teja3668 <kandhularaviteja9949@gmail.com>
2025-02-12 12:49:23 +0530

    Array elements

---
+++ Demo065.java
@@ -0,0 +1,10 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40};
+        System.out.println("Array elements: ");
+        for (int num : numbers){
+            System.out.println(num+" ");
+        }
+    }
+
+}
