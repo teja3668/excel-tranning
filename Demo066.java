1c791316895dfa1f0f7d24f195bd5f7f8de224df
teja3668 <kandhularaviteja9949@gmail.com>
2025-02-12 12:49:44 +0530

    Array index values

---
+++ Demo066.java
@@ -0,0 +1,10 @@
+public class Demo066 {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40};
+        System.out.println("Array index values");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Index"+i+":"+numbers[i]);
+        }
+
+    }
+}
